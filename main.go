fede41edf8a6ff7e5882117d2d0342d006e46111
qi xu <qi.xu@autronicafire.no>
2021-09-21 09:57:57 +0200

    add binary file

--- main.go
+++ main.go
@@ -8,18 +8,19 @@ import (
 
 
 func main (){
-
-	fmt.Println("Hello, Qi")
-	command := exec.Command("/home/qi/Downloads/calcApp", "212", "30")
+	fmt.Println("Hello")
+	command := exec.Command("./calcApp", "245", "30")
 
     // set var to get the output
     var out bytes.Buffer
 
-     // set the output to our variable
-     command.Stdout = &out
-     err := command.Run()
-     if err != nil {
-         fmt.Println(err)
-     }
+    // set the output to our variable
+    command.Stdout = &out
+    err := command.Run()
+    if err != nil {
+        fmt.Println(err)
+    }
     fmt.Println(out.String())
 } 
+
+
